eca74de23e212b1271c37a60d8c49f152d00ae92
skp9n <62747325+skp9n@users.noreply.github.com>
2020-03-27 18:07:08 +0100

    Update create_url.php

--- create_url.php
+++ create_url.php
@@ -5,7 +5,7 @@ $url="https://rotban2.vatsim-germany.org/gen.php?";
 foreach ($_POST as $key => $value) {
     // $arr[3] wird mit jedem Wert von $arr aktualisiert...
 
-    if( $key == "cid" and $vale != ""){
+    if( $key == "cid" and $value != ""){
       $url=$url . "cid=" . $value;
     }
     else {
