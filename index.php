175e3b7bda1f51b5162a494124d4c46b39089d7a
skp9n <62747325+skp9n@users.noreply.github.com>
2020-03-27 19:06:24 +0100

    Update index.php

--- index.php
+++ index.php
@@ -2,10 +2,12 @@
 /* Attempt MySQL server connection. Assuming you are running MySQL
 server with default setting (user 'root' with no password) */
 // Check connection
+$link = require_once('db_conn.php');
+
 if($link === false){
   die("ERROR: Could not connect. " . mysqli_connect_error());
 }
-$link = require_once('db_conn.php');
+
 // Attempt select query execution
 $sql = "SELECT * FROM image where active <> 0";
 $images_result = mysqli_query($link, $sql);
