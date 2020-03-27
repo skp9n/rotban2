298b4115028e56fdaf330bfacf1cf87a1e99796c
skp9n <62747325+skp9n@users.noreply.github.com>
2020-03-27 21:46:25 +0100

    Delete db_conn.php

--- db_conn.php
+++
@@ -1,7 +0,0 @@
-<?php
-<<<<<<< HEAD
-$link = mysqli_connect("localhost", "rotban", "jArYPs2FsoybVBKW", "rotban");
-//fann_test
-=======
-//TODO on Server
->>>>>>> ca1244a6bf0995d7a98f83ae75430bc64445af82
