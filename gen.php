4fd29c9472c16c4a23d7e6b3f528e8f1690e3cda
skp9n <62747325+skp9n@users.noreply.github.com>
2020-03-27 19:15:14 +0100

    Update gen.php

--- gen.php
+++ gen.php
@@ -4,8 +4,22 @@ $cid=$_GET['cid'];
 $images=explode("_",substr($_GET['img'], 1) );
 
 echo "CID , ${cid}\n" ;
-var_dump($images);
+
 
 $random = mt_rand(0,sizeof($images) - 1 );
 
 echo "\n Zuffalszahl, ${random}";
+
+require_once('db_conn.php');
+
+if($link === false){
+  die("ERROR: Could not connect. " . mysqli_connect_error());
+}
+
+// Attempt select query execution
+$imageid = mysqli_real_escape_string($link, $images[$random]);
+$sql = "SELECT * FROM image where id = $imageid";
+
+$images_result = mysqli_query($link, $sql);
+
+var_dump($images_result);
