a582f0c332184f3a68b2d9744abbceae503b4440
skp9n <62747325+skp9n@users.noreply.github.com>
2020-03-27 20:08:52 +0100

    Update index.php

--- index.php
+++ index.php
@@ -86,18 +86,19 @@ mysqli_close($link); ?>
           $.post( "./create_url.php", $( "#rotban_form" ).serializeArray() ).done(function(data) {
             $("#rotban_url").attr("value", data);
             $('#LinkModal').modal('show');
+            var copyText = document.getElementById("rotban_url");
+            /* Select the text field */
+            copyText.select();
+            copyText.setSelectionRange(0, 99999); /*For mobile devices*/
+            /* Copy the text inside the text field */
+            document.execCommand("copy");
+            /* Alert the copied text */
+            alert("Copied the text: " + copyText.value);
           });;
         });
       });
       function copyToClipboard(element) {
-        var copyText = document.getElementById("rotban_url");
-        /* Select the text field */
-        copyText.select();
-        copyText.setSelectionRange(0, 99999); /*For mobile devices*/
-        /* Copy the text inside the text field */
-        document.execCommand("copy");
-        /* Alert the copied text */
-        alert("Copied the text: " + copyText.value);
+
       }
       </script>
     </body>
