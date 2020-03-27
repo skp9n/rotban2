2ee27da5cbc174f246a5ade753dc1b9b016b2b8d
skp9n <62747325+skp9n@users.noreply.github.com>
2020-03-27 12:27:29 +0100

    fix
    fix

--- index.php
+++ index.php
@@ -1,4 +1,4 @@
-<?php <
+<?php 
 /* Attempt MySQL server connection. Assuming you are running MySQL
 server with default setting (user 'root' with no password) */
 $link = mysqli_connect("localhost", "rotban", "KYNfRWbKbTiMHEJW", "rotban");
