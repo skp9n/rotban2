d11b997b752fd5ae804d685737c7ebc8e7799289
skp9n <62747325+skp9n@users.noreply.github.com>
2020-03-27 19:00:46 +0100

    Update gen.php

--- gen.php
+++ gen.php
@@ -6,6 +6,6 @@ $images=explode("_",substr($_GET['img'], 1) );
 echo "CID , ${cid}\n" ;
 var_dump($images);
 
-$random = mt_rand(0,sizeof($images - 1));
+$random = mt_rand(0,sizeof($images) - 1 );
 
 echo "\n Zuffalszahl, ${random}";
