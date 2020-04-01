49205010d7f1090ea24c04681bdc26fefd2eca24
skp9n <62747325+skp9n@users.noreply.github.com>
2020-04-01 16:28:50 +0200

    refactor

--- index.php
+++ index.php
@@ -99,7 +99,7 @@ mysqli_close($link);
         </div>
     </div>
     <div class="row">
-        <div class="col text-break text-center align-content-center">
+        <div class="col form-row align-items-center">
             <input class="form-control mt-3 w-25 text-center" type="text" id="cid" name="cid" value=""
                    placeholder="VATSIM-ID" inputmode="numeric" minlength="6" maxlength="7">
 
