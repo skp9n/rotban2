66c0fe69074da5a793f9210ab4d4330d6199eabb
skp9n <62747325+skp9n@users.noreply.github.com>
2020-04-01 13:36:03 +0200

    refactor

--- index.php
+++ index.php
@@ -32,7 +32,7 @@ mysqli_close($link); ?>
 </head>
 
 <body style="background-color: #2b3089;color: #b9b8b8">
-<div class="container ">
+<div class="container  border-3 border-dark rounded">
     <div class="row">
         <div class="col">
             <img src="assets/img/vacc_logo_white.png" class="mx-auto d-block pt-5" style="max-width: 5cm">
@@ -45,7 +45,7 @@ mysqli_close($link); ?>
         </div>
     </div>
     <div class="row">
-        <div class="col-2 border-3 border-dark rounded">
+        <div class="col-2">
             <ul class="nav flex-column">
                 <li class="nav-item">
                     <a class="nav-link active" href="#">Active</a>
