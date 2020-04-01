0045e38d0882131b1c7c80670ceeaf23a3c3b385
skp9n <62747325+skp9n@users.noreply.github.com>
2020-04-01 11:19:04 +0200

    Update index.php

--- index.php
+++ index.php
@@ -54,13 +54,20 @@ mysqli_close($link); ?>
           <?php
           if(mysqli_num_rows($images_result) > 0){
             while($row = mysqli_fetch_array($images_result)){
+
+              if($row['uri_preview']==NULL or $row['uri_preview'] == ""){
+                $uri=$row['uri'];
+              }
+              else{
+                $uri=$row['uri_preview'];
+              }
               echo "<tr>";
               echo '<td style="color: #b9b8b8;padding:1cm; width: 10%;"><div class="form-check"><input class="form-check-input" type="checkbox" name=' . $row['id'] . ' id="img' . $row['id'] . '" value="0"><label class="form-check-label" data-toggle="tooltip" data-bs-tooltip="" data-placement="right" for="formCheck-1">' . $row['description'] . '</label></div></td>';
               if($row['cid_required']!=0){
-                echo '<td><img max-height="80px" max-width="400px" src="' . str_replace("\$cid","",$row['uri']) . '"/></td>';
+                echo '<td><img max-height="80px" max-width="400px" src="' . str_replace("\$cid","",$uri . '"/></td>';
               }
               else {
-                echo '<td><img max-height="80px" max-width="400px" src="' . $row['uri'] . '"/></td>';
+                echo '<td><img max-height="80px" max-width="400px" src="' . $uri . '"/></td>';
               }
               echo "</tr>";
             }
