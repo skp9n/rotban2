19451f2d13b2ccbd9fab50b48cd799cc0d42d128
skp9n <62747325+skp9n@users.noreply.github.com>
2020-04-01 13:22:19 +0200

    refactor

--- index.php
+++ index.php
@@ -45,18 +45,18 @@ mysqli_close($link); ?>
         </div>
     </div>
     <div class="row">
-        <div class="col-2"
-        <ul class="nav flex-column">
-            <li class="nav-item">
-                <a class="nav-link active" href="#">Active</a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" href="#">Link</a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" href="#">Link</a>
-            </li>
-            <li class="nav-item">
+        <div class="col-2">
+            <ul class="nav flex-column">
+                <li class="nav-item">
+                    <a class="nav-link active" href="#">Active</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Link</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Link</a>
+                </li>
+                <li class="nav-item">
                 <a class="nav-link disabled" href="#">Disabled</a>
             </li>
         </ul>
