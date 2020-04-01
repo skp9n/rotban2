1644182c0b3acf6af9a81bf664fa4f9a66a2054a
skp9n <62747325+skp9n@users.noreply.github.com>
2020-04-01 15:31:48 +0200

    refactor

--- index.php
+++ index.php
@@ -1,6 +1,8 @@
 <?php
 /* Attempt MySQL server connection. Assuming you are running MySQL
 server with default setting (user 'root' with no password) */
+
+
 // Check connection
 require_once('db_conn.php');
 
@@ -17,7 +19,9 @@ $sql = "SELECT * FROM `group` where exists ( select * from `image` where `id_gro
 $groups_result = mysqli_query($link, $sql);
 
 // Close connection
-mysqli_close($link); ?>
+mysqli_close($link);
+
+?>
 
 <!DOCTYPE html>
 <html>
@@ -31,7 +35,7 @@ mysqli_close($link); ?>
 </head>
 
 <body>
-<div class="container">
+<div class="container ">
     <div class="row">
         <div class="col">
             <img src="assets/img/vacc_logo_white.png" class="mx-auto d-block pt-5" style="max-width: 5cm">
@@ -44,25 +48,49 @@ mysqli_close($link); ?>
         </div>
     </div>
     <div class="row">
-        <div class="col bg-secondary rounded justify-content-center">
-            <ul class="nav justify-content-center">
-                <li class="nav-item">
-                    <a class="nav-link active" href="#">Active</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="#">Link</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="#">Link</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link disabled" href="#">Disabled</a>
-                </li>
-            </ul>
-            </ul>
+        <div class="col-2">
+            <div class="container pt-5 sticky">
+                <nav class="nav flex-column flex-fill border-3 rounded">
+
+                    <a class="nav-link active font-weight-bold text-secondary" href="#">TOP</a>
+
+                    <?php
+                    while ($row = mysqli_fetch_array($groups_result)) {
+                        echo '<a class="nav-link active font-weight-bold text-secondary" href="#">' . $row['description'] . '</a>';
+                    }
+                    ?>
+
+                </nav>
+            </div>
         </div>
         <div class="col">
+            <?php
+            while ($group = mysqli_fetch_array($groups_result)) {
+                echo '<div class="container mt-5 justify-content-center" style="min-height: 10cm">';
+                echo '<h3>' . $group['description'] . '</h3>';
+                echo '<div class="table-responsive"><table class="table table-borderless w-auto"><tbody>';
+                while ($image = mysqli_fetch_array($images_result)) {
+                    echo '<tr>';
+                    echo '<td><div class="form-check text-center">';
+                    echo '<input class="form-check-input" type="checkbox" name=' . $image['id'] . ' id="img' . $image['id'] . '" value="0"><label class="form-check-label" for="formCheck-1">' . $image['description'] . '</label>';
+                    echo '</div></td>';
+
+                    if ($image['uri_preview'] == NULL or $image['uri_preview'] == "") {
+                        $uri = $image['uri'];
+                    } else {
+                        $uri = $image['uri_preview'];
+                    }
+
+                    if ($image['cid_required'] != 0) {
+                        echo '<td><img max-height="80px" max-width="400px" src="' . str_replace("\$cid", "", $uri) . '"/></td>';
+                    } else {
+                        echo '<td><img max-height="80px" max-width="400px" src="' . $uri . '"/></td>';
+                    }
+                    echo '</tr>';
+                }
+                echo '</tbody> </table></div></div>';
+            }
+            ?>
         </div>
     </div>
-</div>
 </body>
