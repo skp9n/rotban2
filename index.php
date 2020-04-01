bd7d3916c4a5e44365826a58d0da84188300b3b0
skp9n <62747325+skp9n@users.noreply.github.com>
2020-04-01 16:43:40 +0200

    refactor

--- index.php
+++ index.php
@@ -98,11 +98,11 @@ mysqli_close($link);
             ?>
         </div>
     </div>
-    <div class="row">
+    <div class="row align-items-center">
         <div class="col-3 justify-content-center align-items-center">
-                <input class="form-control mt-3 text-center" type="text" id="cid" name="cid" value=""
-                       placeholder="VATSIM-ID"
-                       inputmode="numeric" minlength="6" maxlength="7">
+            <input class="form-control mt-3 text-center" type="text" id="cid" name="cid" value=""
+                   placeholder="VATSIM-ID"
+                   inputmode="numeric" minlength="6" maxlength="7">
             <button class="btn btn-success btn-lg mt-3" type="submit">Rotbanlink generieren</button>
         </div>
     </div>
