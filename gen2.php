52aaaf05b8a3fd9c6a8345a847449d3093ef456d
skp9n <62747325+skp9n@users.noreply.github.com>
2020-04-02 10:02:13 +0200

    v2.0

---
+++ gen2.php
@@ -0,0 +1,39 @@
+<?php
+
+//$cid=$_GET['cid'];
+//$images=explode("_",substr($_GET['img'], 1) );
+
+$cid = "1331358";
+$images = $images = explode("_", substr("https://rotban2.vatsim-germany.org/gen.php?img=_6", 1));
+
+//echo $images;
+
+$random = mt_rand(0, sizeof($images) - 1);
+
+require_once('db_conn.php');
+
+if ($link === false) {
+    die("ERROR: Could not connect. " . mysqli_connect_error());
+}
+
+// Attempt select query execution
+$imageid = mysqli_real_escape_string($link, $images[$random]);
+$sql = "SELECT `cid_required`, `uri` FROM image where id = $imageid";
+$images_result = mysqli_query($link, $sql);
+
+// Close connection
+mysqli_close($link);
+
+if (mysqli_num_rows($images_result) > 0) {
+    while ($row = mysqli_fetch_array($images_result)) {
+        //echo $row['uri'];
+        $uri = str_replace("\$cid", urlencode($cid), $row['uri']);
+        echo "Uri: ${uri}";
+
+        echo mime_content_type($uri);
+        //header("Location: " . str_replace("\$cid", urlencode($cid), $row['uri']));
+    }  // Free result se
+} else {
+    // code...
+}
+mysqli_free_result($images_result);
